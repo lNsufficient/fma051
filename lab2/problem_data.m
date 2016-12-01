c40e46a53d74c7ed1db1635a4a1ff1f41ef387e9
Edvard Johansson <edvard.jo@gmail.com>
2016-12-01 21:04:34 +0100

    lade till så att x_k >= 0 i problem_data

--- lab2/problem_data.m
+++ lab2/problem_data.m
@@ -9,4 +9,12 @@ function [A,b,f] = problem_data
 
 A = [1 3 2; 1 1 1; 3 5 3]';
 b = [2 4 3]';
+
+%To make sure that x_k >= 0;
+Aones = eye(3);
+bones = zeros(3,1);
+A = [A; Aones];
+b = [b; bones];
+
+
 f = @(x) dot([30, 24, 60], x);
