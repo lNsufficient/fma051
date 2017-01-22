b9b61b3d4b22cf4ab8633a45e406dedb5e48b30e
lNsufficient <edvard.jo@gmail.com>
2017-01-22 13:09:31 +0100

    ändrade i testet

--- minip/test_gaussian_starting_points.m
+++ minip/test_gaussian_starting_points.m
@@ -5,20 +5,42 @@ opt = 8.9616;
 sigmas = [0.1, 1,2,5,10, 20, 50, 100, 200, 500, 1000];
 sigmas = linspace(0.01, 50, 20)
 opt_x  = [4.1742; 0.8748; 9.7389; 2.9208];
+opt_x2 = [9.7389; 2.9208; 4.1742; 0.8748];
 samples = 100;
 sucess = zeros(size(sigmas));
+
+
+nbr_runs = length(sigmas)*samples;
+distances = ones(nbr_runs, 1)*NaN;
+f_vals = distances;
+
+
 for k = 1:length(sigmas)
     sigma = sigmas(k);
     X = mvnrnd(opt_x*ones(1,samples), sigma*ones(1,samples));
     for j = 1:samples
         [x_n, f] = gaussnewton(@phi2,t,y,X(:,j), 0.01,1,0,0)
+        
+        current_ind = (k-1)*samples + j;
+        f_vals(current_ind) = abs(f-opt);
+        distances(current_ind) = min(norm(x_n-opt_x),norm(x_n-opt_x2));
+        
         if abs(f-opt) < 0.1
             sucess(k) = sucess(k) +1;
         end
     end
 end
 
+figure(1)
 plot(sigmas, sucess/samples)
 title('Stability of program for Gaussian starting points around minima')
 xlabel('Standard deviation of parameters (x-values)')
 ylabel('Percentage of samples which converged to minima')
+
+figure(2)
+max_y = 200;
+semilogx(distances, min(max_y, f_vals), '.');
+title('Plot som visar prestanda i relation till startpunkt')
+xlabel('Startpunktens minsta avstånd till någon optimal punkt')
+ylabel('Absolutvärde av det funna f-värdet subraherat från det optimala')
+
